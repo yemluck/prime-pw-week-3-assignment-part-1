ddf76a438aba08c1f057b2e42545f4cb291b1733
Chris Agbolade <yemluck@gmail.com>
2021-11-11 02:24:39 -0600

    Question 3 done

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -34,11 +34,11 @@ console.log('First animal is', animalArray[0]);
 console.log('Second animal is', animalArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index
-
+console.log('Last animal is', animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length,
 //      instead of the exact index number of the last item
-
+console.log('Last animal is', animalArray[animalArray.length - 1]);
 
 
 // 4. Adding & Removing Array Items
